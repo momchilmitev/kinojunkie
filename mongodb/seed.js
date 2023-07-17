292d65e21ade318e88e8aee7d4956acf45753d38
momchilmitev <momchilmitev87@gmail.com>
2023-07-17 15:00:00 +0200

    added some extra seed data for the movies collection

--- mongodb/seed.js
+++ mongodb/seed.js
@@ -1,8 +1,122 @@
 db.movies.insertMany([
   {
     title: 'Inception',
-    poster: 'https://www.dropbox.com/s/3lqk6fo7chiyycp/inception.jpeg?raw=1',
+    poster_small:
+      'https://www.dropbox.com/scl/fi/gitcmjve9trhy9v066vyn/inception-mittel.jpeg?rlkey=7evlwd6y8meevcrukqibpnbwl&raw=1',
+    poster_large:
+      'https://www.dropbox.com/scl/fi/ed61mad3epmkmtt1i4tm6/inception-gro.jpeg?rlkey=0694pr49sxj4ego0yarj16dbn&raw=1',
     year: 2010,
     duration: 148,
+    price: 6.99,
+  },
+  {
+    title: 'Aquaman',
+    poster_small:
+      'https://www.dropbox.com/scl/fi/urwmv4sgi8vm0i7nx703n/aquaman-mittel.jpeg?rlkey=p0872t0yqh4djhcrcysg9v2va&raw=1',
+    poster_large:
+      'https://www.dropbox.com/scl/fi/g86bxn2jfbk1j5sg8saj7/aquaman-gro.jpeg?rlkey=4if4fpftuiz200j6mipn4akas&raw=1',
+    year: 2018,
+    duration: 143,
+    price: 9.99,
+  },
+  {
+    title: 'Avengers Endgame',
+    poster_small:
+      'https://www.dropbox.com/scl/fi/wewavx63wtd63l7xaeati/avengers-endgame-mittel.jpeg?rlkey=07gpjiy9c1t6m4kirerd7wtnu&raw=1',
+    poster_large:
+      'https://www.dropbox.com/scl/fi/1gxwaaob6334idfp09xis/avengers-endgame-gro.jpeg?rlkey=y2t5nz6rfw2vbkl0bcn6c15ql&raw=1',
+    year: 2019,
+    duration: 181,
+    price: 9.99,
+  },
+  {
+    title: 'Black Widow',
+    poster_small:
+      'https://www.dropbox.com/scl/fi/wewavx63wtd63l7xaeati/black-widow-mittel.jpeg?rlkey=07gpjiy9c1t6m4kirerd7wtnu&raw=1',
+    poster_large:
+      'https://www.dropbox.com/scl/fi/1gxwaaob6334idfp09xis/black-widow-gro.jpeg?rlkey=y2t5nz6rfw2vbkl0bcn6c15ql&raw=1',
+    year: 2021,
+    duration: 134,
+    price: 12.99,
+  },
+  {
+    title: 'Godzilla',
+    poster_small:
+      'https://www.dropbox.com/scl/fi/wewavx63wtd63l7xaeati/godzilla-mittel.jpeg?rlkey=07gpjiy9c1t6m4kirerd7wtnu&raw=1',
+    poster_large:
+      'https://www.dropbox.com/scl/fi/1gxwaaob6334idfp09xis/godzilla-gro.jpeg?rlkey=y2t5nz6rfw2vbkl0bcn6c15ql&raw=1',
+    year: 2014,
+    duration: 123,
+    price: 8.99,
+  },
+  {
+    title: 'Godzilla vs Kong',
+    poster_small:
+      'https://www.dropbox.com/scl/fi/wewavx63wtd63l7xaeati/godzilla-vs-kong-mittel.jpeg?rlkey=07gpjiy9c1t6m4kirerd7wtnu&raw=1',
+    poster_large:
+      'https://www.dropbox.com/scl/fi/1gxwaaob6334idfp09xis/godzilla-vs-kong-gro.jpeg?rlkey=y2t5nz6rfw2vbkl0bcn6c15ql&raw=1',
+    year: 2021,
+    duration: 113,
+    price: 10.99,
+  },
+  {
+    title: 'John Wick',
+    poster_small:
+      'https://www.dropbox.com/scl/fi/wewavx63wtd63l7xaeati/john-wick-mittel.jpeg?rlkey=07gpjiy9c1t6m4kirerd7wtnu&raw=1',
+    poster_large:
+      'https://www.dropbox.com/scl/fi/1gxwaaob6334idfp09xis/john-wick-gro.jpeg?rlkey=y2t5nz6rfw2vbkl0bcn6c15ql&raw=1',
+    year: 2014,
+    duration: 101,
+    price: 7.99,
+  },
+  {
+    title: 'Joker',
+    poster_small:
+      'https://www.dropbox.com/scl/fi/wewavx63wtd63l7xaeati/joker-mittel.jpeg?rlkey=07gpjiy9c1t6m4kirerd7wtnu&raw=1',
+    poster_large:
+      'https://www.dropbox.com/scl/fi/1gxwaaob6334idfp09xis/joker-gro.jpeg?rlkey=y2t5nz6rfw2vbkl0bcn6c15ql&raw=1',
+    year: 2019,
+    duration: 122,
+    price: 9.99,
+  },
+  {
+    title: 'Suicide Squad',
+    poster_small:
+      'https://www.dropbox.com/scl/fi/wewavx63wtd63l7xaeati/suicide-squad-mittel.jpeg?rlkey=07gpjiy9c1t6m4kirerd7wtnu&raw=1',
+    poster_large:
+      'https://www.dropbox.com/scl/fi/1gxwaaob6334idfp09xis/suicide-squad-gro.jpeg?rlkey=y2t5nz6rfw2vbkl0bcn6c15ql&raw=1',
+    year: 2016,
+    duration: 123,
+    price: 8.99,
+  },
+  {
+    title: 'The Last Jedi',
+    poster_small:
+      'https://www.dropbox.com/scl/fi/wewavx63wtd63l7xaeati/the-last-jedi-mittel.jpeg?rlkey=07gpjiy9c1t6m4kirerd7wtnu&raw=1',
+    poster_large:
+      'https://www.dropbox.com/scl/fi/1gxwaaob6334idfp09xis/the-last-jedi-gro.jpeg?rlkey=y2t5nz6rfw2vbkl0bcn6c15ql&raw=1',
+    year: 2017,
+    duration: 152,
+    price: 7.99,
+  },
+  {
+    title: 'The Lion King',
+    poster_small:
+      'https://www.dropbox.com/scl/fi/wewavx63wtd63l7xaeati/the-lion-king-mittel.jpeg?rlkey=07gpjiy9c1t6m4kirerd7wtnu&raw=1',
+    poster_large:
+      'https://www.dropbox.com/scl/fi/1gxwaaob6334idfp09xis/the-lion-king-gro.jpeg?rlkey=y2t5nz6rfw2vbkl0bcn6c15ql&raw=1',
+    year: 2019,
+    duration: 118,
+    price: 9.99,
+  },
+  {
+    title: 'The Matrix Resurrections',
+    poster_small:
+      'https://www.dropbox.com/scl/fi/wewavx63wtd63l7xaeati/the-matrix-resurrections-mittel.jpeg?rlkey=07gpjiy9c1t6m4kirerd7wtnu&raw=1',
+    poster_large:
+      'https://www.dropbox.com/scl/fi/1gxwaaob6334idfp09xis/the-matrix-resurrections-gro.jpeg?rlkey=y2t5nz6rfw2vbkl0bcn6c15ql&raw=1',
+    year: 2021,
+    duration: 148,
+    price: 12.99,
   },
 ]);
