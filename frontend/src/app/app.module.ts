279ea22a48ff4a3fcd57e6915e0f590a955a8d2b
momchilmitev <momchilmitev87@gmail.com>
2023-07-23 13:31:05 +0200

    added the ngrx to the project

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -8,6 +8,9 @@ import { GridModule } from './core/grid/grid.module'
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CardModule } from './shared/card/card.module';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 @NgModule({
   declarations: [
     AppComponent,
@@ -19,7 +22,10 @@ import { CardModule } from './shared/card/card.module';
     BrowserModule,
     GridModule,
     HttpClientModule,
-    CardModule
+    CardModule,
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() })
   ],
   providers: [],
   bootstrap: [AppComponent]
