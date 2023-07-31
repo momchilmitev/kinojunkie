c51652b735cf35836788d86cf7ce2c442b72a560
momchilmitev <momchilmitev87@gmail.com>
2023-07-31 21:34:00 +0300

    changed the logic for https certs

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,27 +1,12 @@
-import mkcert from 'mkcert';
+import { readFileSync } from 'node:fs';
+import { resolve } from 'node:path';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const ca = await mkcert.createCA({
-    organization: 'Hello CA',
-    countryCode: 'NP',
-    state: 'Bagmati',
-    locality: 'Kathmandu',
-    validityDays: 365,
-  });
-
-  // then create a tls certificate
-  const cert = await mkcert.createCert({
-    domains: ['127.0.0.1', 'localhost', '0.0.0.0'],
-    validityDays: 365,
-    caKey: ca.key,
-    caCert: ca.cert,
-  });
-
   const httpsOptions = {
-    key: cert.key,
-    cert: cert.cert,
+    key: readFileSync(resolve(__dirname, 'assets/cert.key')),
+    cert: readFileSync(resolve(__dirname, 'assets/cert.crt')),
   };
 
   const app = await NestFactory.create(AppModule, { httpsOptions });
