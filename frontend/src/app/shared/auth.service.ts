a6d40954c36c32adc7e0967e63e81d4a4ec8708f
momchilmitev <momchilmitev87@gmail.com>
2023-07-31 18:04:03 +0300

    changed backend url in frontend services

--- frontend/src/app/shared/auth.service.ts
+++ frontend/src/app/shared/auth.service.ts
@@ -9,14 +9,14 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   register(userData: any) {
-    return this.http.post('http://0.0.0.0:3000/auth/register', userData)
+    return this.http.post('http://64.225.108.81:3000/auth/register', userData)
   }
 
   login(userData: any) {
-    return this.http.post<{access_token: string}>('http://0.0.0.0:3000/auth/login', userData)
+    return this.http.post<{access_token: string}>('http://64.225.108.81:3000/auth/login', userData)
   }
 
   updateUser(userData: any) {
-    return this.http.put('http://0.0.0.0:3000/auth/profile', userData)
+    return this.http.put('http://64.225.108.81:3000/auth/profile', userData)
   }
 }
