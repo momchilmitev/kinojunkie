50f1214f00e1e86c418465ef75d6c541564627f3
momchilmitev <momchilmitev87@gmail.com>
2023-07-31 20:09:08 +0300

    added https to the server

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,30 @@
+import mkcert from 'mkcert';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const ca = await mkcert.createCA({
+    organization: 'Hello CA',
+    countryCode: 'NP',
+    state: 'Bagmati',
+    locality: 'Kathmandu',
+    validityDays: 365,
+  });
+
+  // then create a tls certificate
+  const cert = await mkcert.createCert({
+    domains: ['127.0.0.1', 'localhost', '0.0.0.0'],
+    validityDays: 365,
+    caKey: ca.key,
+    caCert: ca.cert,
+  });
+
+  const httpsOptions = {
+    key: cert.key,
+    cert: cert.cert,
+  };
+
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   app.enableCors();
   await app.listen(3000, '0.0.0.0');
 }
