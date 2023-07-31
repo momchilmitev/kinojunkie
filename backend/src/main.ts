11a7c22002d54445fb245f4e78732a4f7dbcec58
momchilmitev <momchilmitev87@gmail.com>
2023-07-31 22:12:18 +0300

    removed the https settings

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,15 +1,8 @@
-import { readFileSync } from 'node:fs';
-import { resolve } from 'node:path';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const httpsOptions = {
-    key: readFileSync(resolve(__dirname, 'assets/cert.key')),
-    cert: readFileSync(resolve(__dirname, 'assets/cert.crt')),
-  };
-
-  const app = await NestFactory.create(AppModule, { httpsOptions });
+  const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(3000, '0.0.0.0');
 }
