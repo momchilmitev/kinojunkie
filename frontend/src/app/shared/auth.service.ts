038835e3fbe1321aaf4e46ac2d694cb79c32b1e9
momchilmitev <momchilmitev87@gmail.com>
2023-07-31 19:46:38 +0300

    added url part to allow https to http requests

--- frontend/src/app/shared/auth.service.ts
+++ frontend/src/app/shared/auth.service.ts
@@ -9,14 +9,14 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   register(userData: any) {
-    return this.http.post('http://64.225.108.81:3000/auth/register', userData)
+    return this.http.post('https://cors-anywhere.herokuapp.com/http://64.225.108.81:3000/auth/register', userData)
   }
 
   login(userData: any) {
-    return this.http.post<{access_token: string}>('http://64.225.108.81:3000/auth/login', userData)
+    return this.http.post<{access_token: string}>('https://cors-anywhere.herokuapp.com/http://64.225.108.81:3000/auth/login', userData)
   }
 
   updateUser(userData: any) {
-    return this.http.put('http://64.225.108.81:3000/auth/profile', userData)
+    return this.http.put('https://cors-anywhere.herokuapp.com/http://64.225.108.81:3000/auth/profile', userData)
   }
 }
